fb988423e189dc6ec68dcbe8d5c15c4e9bd2a5fb
xing <xing.wang@forceclouds.com>
2018-07-31 16:51:53 +0800

    LeetCode 初级算法 数组 1-5

---
+++ src/main/java/com/leetcode/junior/array/Array3.java
@@ -0,0 +1,30 @@
+package com.leetcode.junior.array;
+
+import java.util.Arrays;
+
+/**
+ *  给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
+ *
+ *  输入: [1,2,3,4,5,6,7] 和 k = 3
+ *  输出: [5,6,7,1,2,3,4]
+ */
+public class Array3 {
+    public static void main(String[] args) {
+        int[] a = {1,2,3,4,5,6,7};
+        int[] b = {-1,-100,3,99};
+        rotate(b,2);
+    }
+
+    public static void rotate(int[] nums, int k) {
+        if (nums.length == 0) return;
+        for (int j = 0; j < k; j++) {
+            int prev = nums[nums.length - 1];
+            for (int i = 0; i < nums.length; i++) {
+                int temp = nums[i];
+                nums[i] = prev;
+                prev = temp;
+            }
+        }
+        System.out.println(Arrays.toString(nums));
+    }
+}
---
+++ src/main/java/com/leetcode/junior/array/Array4.java
@@ -0,0 +1,24 @@
+package com.leetcode.junior.array;
+
+/**
+ *  给定一个整数数组，判断是否存在重复元素。
+ *
+ *  如果任何值在数组中出现至少两次，函数返回 true。如果数组中每个元素都不相同，则返回 false。
+ */
+public class Array4 {
+
+    public static void main(String[] args) {
+        int[] a = {1,2,3,4,5,6,1};
+        System.out.println(containsDuplicate(a));
+    }
+
+    public static boolean containsDuplicate(int[] nums) {
+        // 采用暴力破解 可以使用Set 和Arrays.sort()方法进行操作
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) return true;
+            }
+        }
+        return false;
+    }
+}
