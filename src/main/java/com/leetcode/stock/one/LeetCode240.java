4f8976708cd26e1b133b62237003434a746dc5cf
fangchunzao <fangchunzao@outlook.com>
2019-08-16 09:55:51 +0800

    240. 搜索二维矩阵 II

---
+++ src/main/java/com/leetcode/stock/one/LeetCode240.java
@@ -0,0 +1,137 @@
+package com.leetcode.stock.one;
+
+/**
+ * 240. 搜索二维矩阵 II
+ *
+ * 编写一个高效的算法来搜索 m x n 矩阵 matrix 中的一个目标值 target。该矩阵具有以下特性：
+ *
+ * 每行的元素从左到右升序排列。
+ * 每列的元素从上到下升序排列。
+ * 示例:
+ *
+ * 现有矩阵 matrix 如下：
+ *
+ * [
+ *   [1,   4,  7, 11, 15],
+ *   [2,   5,  8, 12, 19],
+ *   [3,   6,  9, 16, 22],
+ *   [10, 13, 14, 17, 24],
+ *   [18, 21, 23, 26, 30]
+ * ]
+ * 给定 target = 5，返回 true。
+ *
+ * 给定 target = 20，返回 false。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/search-a-2d-matrix-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ */
+public class LeetCode240 {
+
+    public static void main(String[] args) {
+
+    }
+
+    // 二分查找
+    // 每一行 每一列都进行二分查找
+    public boolean searchMatrix(int[][] matrix, int target) {
+        // an empty matrix obviously does not contain `target`
+        if (matrix == null || matrix.length == 0) {
+            return false;
+        }
+
+        int shorterDim = Math.min(matrix.length, matrix[0].length);
+        for (int i = 0; i < shorterDim; i++) {
+            // 二分查找列
+            boolean verticalFound = binarySearch(matrix, target, i, true);
+            // 二分查找行
+            boolean horizontalFound = binarySearch(matrix, target, i, false);
+            if (verticalFound || horizontalFound) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private boolean searchMatrix(int[][] matrix, int target, int start, boolean vertical) {
+        // 确定边界
+        int lo = start;
+        int hi = vertical ? matrix[0].length - 1: matrix.length - 1;
+
+        while (lo < hi) {
+            int mid = (hi + lo) / 2;
+            // 判断是行二分查找 还是列二分查找
+            if (vertical) {
+                // 二分查找列
+                if (matrix[start][mid] < target) {
+                    lo = mid + 1;
+                } else  if(matrix[start][mid] > target){
+                    hi = mid - 1;
+                } else {
+                    return true;
+                }
+            } else {
+                // 二分查找行
+                if (matrix[mid][start] < target) {
+                    lo = mid + 1;
+                } else if (matrix[mid][start] > target) {
+                    hi = mid - 1;
+                } else {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean binarySearch(int[][] matrix, int target, int start, boolean vertical) {
+        int lo = start;
+        int hi = vertical ? matrix[0].length-1 : matrix.length-1;
+
+        while (hi >= lo) {
+            int mid = (lo + hi)/2;
+            if (vertical) { // searching a column
+                if (matrix[start][mid] < target) {
+                    lo = mid + 1;
+                } else if (matrix[start][mid] > target) {
+                    hi = mid - 1;
+                } else {
+                    return true;
+                }
+            } else { // searching a row
+                if (matrix[mid][start] < target) {
+                    lo = mid + 1;
+                } else if (matrix[mid][start] > target) {
+                    hi = mid - 1;
+                } else {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    // 最优解  从最左下角开始查找
+    public boolean searchMatrix1(int[][] matrix, int target) {
+        // start our "pointer" in the bottom-left
+        int row = matrix.length-1;
+        int col = 0;
+
+        while (row >= 0 && col < matrix[0].length) {
+            if (matrix[row][col] > target) {
+                row--;
+            } else if (matrix[row][col] < target) {
+                col++;
+            } else { // found it
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+}
