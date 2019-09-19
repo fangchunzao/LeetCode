da7566fa75fa253ccaf9da5a9a8b8ad71e195339
fangchunzao <fangchunzao@outlook.com>
2019-09-19 11:06:59 +0800

    239. 滑动窗口最大值 (动态规划 && 优先队列 最大堆)

---
+++ src/main/java/com/leetcode/stock/one/LeetCode239.java
@@ -0,0 +1,94 @@
+package com.leetcode.stock.one;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+
+/**
+ * 239. 滑动窗口最大值
+ *
+ * 给定一个数组 nums，有一个大小为 k 的滑动窗口从数组的最左侧移动到数组的最右侧。你只可以看到在滑动窗口内的 k 个数字。滑动窗口每次只向右移动一位。
+ *
+ * 返回滑动窗口中的最大值。
+ *
+ *  
+ *
+ * 示例:
+ *
+ * 输入: nums = [1,3,-1,-3,5,3,6,7], 和 k = 3
+ * 输出: [3,3,5,5,6,7]
+ * 解释:
+ *
+ *   滑动窗口的位置                最大值
+ * ---------------               -----
+ * [1  3  -1] -3  5  3  6  7       3
+ *  1 [3  -1  -3] 5  3  6  7       3
+ *  1  3 [-1  -3  5] 3  6  7       5
+ *  1  3  -1 [-3  5  3] 6  7       5
+ *  1  3  -1  -3 [5  3  6] 7       6
+ *  1  3  -1  -3  5 [3  6  7]      7
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/sliding-window-maximum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author Mr.F
+ * @since 2019/9/19 10:25
+ **/
+public class LeetCode239 {
+
+    public static void main(String[] args) {
+        LeetCode239 obj = new LeetCode239();
+        obj.maxSlidingWindow(new int[] {1,3,-1,-3,5,3,6,7}, 3);
+    }
+
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        PriorityQueue<Integer> queue = new PriorityQueue<>((n1,n2) -> n2-n1);
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0, j = 0; i < nums.length; i++) {
+            queue.add(nums[i]);
+            if (queue.size() >= k) {
+                // 获取优先队列中最大的元素
+                list.add(queue.peek());
+                // 删除最前面的元素
+                queue.remove(nums[j++]);
+
+            }
+        }
+        int[] result = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            result[i] = list.get(i);
+        }
+        return result;
+    }
+
+    // 动态规划
+    // https://leetcode-cn.com/problems/sliding-window-maximum/solution/hua-dong-chuang-kou-zui-da-zhi-by-leetcode-3/
+    public int[] maxSlidingWindow1(int[] nums, int k) {
+        int n = nums.length;
+        if (n * k == 0) return new int[0];
+        if (k == 1) return nums;
+
+        int [] left = new int[n];
+        left[0] = nums[0];
+        int [] right = new int[n];
+        right[n - 1] = nums[n - 1];
+        for (int i = 1; i < n; i++) {
+            // from left to right
+            if (i % k == 0) left[i] = nums[i];  // block_start
+            else left[i] = Math.max(left[i - 1], nums[i]);
+
+            // from right to left
+            int j = n - i - 1;
+            if ((j + 1) % k == 0) right[j] = nums[j];  // block_end
+            else right[j] = Math.max(right[j + 1], nums[j]);
+        }
+
+        int [] output = new int[n - k + 1];
+        for (int i = 0; i < n - k + 1; i++)
+            output[i] = Math.max(left[i + k - 1], right[i]);
+
+        return output;
+    }
+
+}
