fc623e49ad1031293cdfc444c0275a394c830ccf
chunzao <fangchunzao@outlook.com>
2020-03-03 23:14:02 +0800

    切绳子 dp

---
+++ src/main/java/com/leetcode/iv/offer/Offer13.java
@@ -0,0 +1,106 @@
+package com.leetcode.iv.offer;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 面试题13. 机器人的运动范围
+ *
+ * 地上有一个m行n列的方格，从坐标 [0,0] 到坐标 [m-1,n-1] 。一个机器人从坐标 [0, 0] 的格子开始移动，
+ * 它每次可以向左、右、上、下移动一格（不能移动到方格外），也不能进入行坐标和列坐标的数位之和大于k的格子。
+ *
+ * 例如，当k为18时，机器人能够进入方格 [35, 37] ，因为3+5+3+7=18。
+ * 但它不能进入方格 [35, 38]，因为3+5+3+8=19。
+ *
+ * 请问该机器人能够到达多少个格子？
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：m = 2, n = 3, k = 1
+ * 输出：3
+ * 示例 1：
+ *
+ * 输入：m = 3, n = 1, k = 0
+ * 输出：1
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/ji-qi-ren-de-yun-dong-fan-wei-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ */
+public class Offer13 {
+
+    public static void main(String[] args) {
+        Offer13 obj = new Offer13();
+        System.out.println(obj.movingCount(35,37,18));
+    }
+
+    public int movingCount(int m, int n, int k) {
+        matrix = new boolean[m][n];
+        return movingCount(m, n, k, 0, 0);
+    }
+
+    boolean[][] matrix;
+
+    // 广度优先算法 BFS
+    // 向四周寻找
+    public int movingCount(int m, int n, int k, int x, int y) {
+        if (x < 0 || x > m - 1 || y < 0 || y > n - 1 || matrix[x][y])
+            return 0;
+
+        int sum = 0;
+        int tempX = x;
+        while (tempX > 0) {
+            sum += tempX % 10;
+            tempX /= 10;
+        }
+        int tempY = y;
+        while (tempY > 0) {
+            sum += tempY % 10;
+            tempY /= 10;
+        }
+        if (sum > k)
+            return 0;
+        matrix[x][y] = true;
+        int count = 1;
+        count += movingCount(m, n, k, x - 1, y);
+        count += movingCount(m, n, k, x + 1, y);
+        count += movingCount(m, n, k, x, y - 1);
+        count += movingCount(m, n, k, x, y + 1);
+
+        return count;
+    }
+
+    // 深度优先算法DFS 向边界找 进行回溯
+    int m, n, k;
+    boolean[][] visited;
+    public int movingCount1(int m, int n, int k) {
+        this.m = m; this.n = n; this.k = k;
+        this.visited = new boolean[m][n];
+        return dfs(0, 0, 0, 0);
+    }
+
+    /**
+     *
+     * @param i 横坐标
+     * @param j 纵坐标
+     * @param si 用于计算横坐标的数位和
+     * @param sj 用于计算纵坐标的数位和
+     * @return
+     */
+    public int dfs(int i, int j, int si, int sj) {
+        if(i < 0 || i >= m || j < 0 || j >= n || k < si + sj || visited[i][j])
+            return 0;
+
+        visited[i][j] = true;
+        // (i + 1) % 10 != 0 ? si + 1 : si - 8 用于计算横坐标的数位和
+        // (j + 1) % 10 != 0 ? sj + 1 : sj - 8 用于计算纵坐标的数位和
+        return 1 +
+                dfs(i + 1, j, (i + 1) % 10 != 0 ? si + 1 : si - 8, sj) +
+                dfs(i, j + 1, si, (j + 1) % 10 != 0 ? sj + 1 : sj - 8);
+    }
+
+
+}
---
+++ src/main/java/com/leetcode/iv/offer/Offer14_1.java
@@ -0,0 +1,88 @@
+package com.leetcode.iv.offer;
+
+/**
+ * 面试题14- I. 剪绳子
+ *
+ * 给你一根长度为 n 的绳子，请把绳子剪成整数长度的 m 段（m、n都是整数，n>1并且m>1），每段绳子的长度记为 k[0],k[1]...k[m] 。请问 k[0]*k[1]*...*k[m] 可能的最大乘积是多少？例如，当绳子的长度是8时，我们把它剪成长度分别为2、3、3的三段，此时得到的最大乘积是18。
+ *
+ * 示例 1：
+ *
+ * 输入: 2
+ * 输出: 1
+ * 解释: 2 = 1 + 1, 1 × 1 = 1
+ * 示例 2:
+ *
+ * 输入: 10
+ * 输出: 36
+ * 解释: 10 = 3 + 3 + 4, 3 × 3 × 4 = 36
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/jian-sheng-zi-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ */
+public class Offer14_1 {
+
+    int[] memory;
+
+    public static void main(String[] args) {
+        Offer14_1 obj = new Offer14_1();
+
+        System.out.println(obj.integerBreak3(5));
+    }
+
+    public int integerBreak(int n) {
+        memory = new int[n + 1];
+        return integerBreakHelper(n);
+    }
+    public int integerBreakHelper(int n) {
+        if (n == 2) {
+            return 1;
+        }
+        // 记忆化的核心 保存 n 对应的最大长度
+        if (memory[n] != 0) {
+            // memory的初始值为0，如果它不为0，说明已经计算过了，直接返回即可
+            return memory[n];
+        }
+        int res = -1;
+        // i 为分割的长度
+        for (int i = 1; i < n; i++) {
+            // 以i长度 分割后 到 末尾最大值 相乘
+            int sum1 = i * (n - i);
+            // 以i长度 分割后 剩余的长度能分割的最大长度
+            int sum = i * integerBreakHelper(n - i);
+            int maxS = Math.max(sum, sum1);
+            res = Math.max(res, maxS);
+        }
+        memory[n] = res;
+        return res;
+    }
+
+    // 动态规划 由记忆回溯推导出
+    public int integerBreak3(int n) {
+        memory = new int[n + 1];
+        memory[2] = 1;
+        // i 为当前长度
+        for (int i = 3; i <= n; i++) {
+            // j为分割长度
+            for (int j = 1; j <= i - 1; j++) {
+                // j * (i - j)  以 j长度 分割后 到 末尾最大值 相乘
+                // j * memory[i - j] 以i长度 分割后 剩余的长度能分割的最大长度 i-j 的位置保存之前计算好的值
+                memory[i] = Math.max(memory[i], Math.max(j * memory[i - j], j * (i - j)));
+            }
+        }
+        return memory[n];
+    }
+
+
+    // 贪心法 分割长度3 效益最高 尽量分割为3
+    public int cuttingRope1(int n) {
+        if(n <= 3) return n - 1;
+        int a = n / 3,
+                b = n % 3;
+        if(b == 0) return (int)Math.pow(3, a);
+        if(b == 1) return (int)Math.pow(3, a - 1) * 4;
+        return (int)Math.pow(3, a) * 2;
+    }
+
+}
