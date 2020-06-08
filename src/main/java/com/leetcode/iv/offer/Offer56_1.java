c16981f9e431902b96dab2c520c4d39346d152e8
fangchunzao <fangchunzao@outlook.com>
2020-06-08 17:04:58 +0800

    面试题56 - I. 数组中数字出现的次数 面试题56 - II. 数组中数字出现的次数 II

---
+++ src/main/java/com/leetcode/iv/offer/Offer56_1.java
@@ -0,0 +1,62 @@
+package com.leetcode.iv.offer;
+
+/**
+ * description 面试题56 - I. 数组中数字出现的次数
+ *
+ * 一个整型数组 nums 里除两个数字之外，其他数字都出现了两次。请写程序找出这两个只出现一次的数字。
+ * 要求时间复杂度是O(n)，空间复杂度是O(1)。
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [4,1,4,6]
+ * 输出：[1,6] 或 [6,1]
+ * 示例 2：
+ *
+ * 输入：nums = [1,2,10,4,1,4,3,3]
+ * 输出：[2,10] 或 [10,2]
+ *  
+ *
+ * 限制：
+ *
+ * 2 <= nums.length <= 10000
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/shu-zu-zhong-shu-zi-chu-xian-de-ci-shu-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author fcz
+ * @since  2020-06-08 10:25
+ **/
+public class Offer56_1 {
+
+    // 把所有数字异或，得到一个数，这个数必定是两个只出现一次的数字异或得到的
+    // 异或规则是两个相应的bit位相同为0，不同为1
+    // 根据这个任意找一个为1的数位
+    // 根据这个数位为0和1分成两个数组
+    // 这样必定把两个结果数分开了，再分别异或就能得到结果
+    public int[] singleNumbers(int[] nums) {
+        int sum=0;
+        //将数组所有元素进行异或，最后的结果一定是那两个单一数字的异或结果
+        for (int num : nums) {
+            sum ^= num;
+        }
+        int first = 1;
+        //通过与运算找到异或结果中首个1 因为异或结果中1代表两数字二进制该位不同
+        while((sum & first)==0){
+            first=first<<1;
+        }
+        // 我们可以根据数组元素的二进制低位第一位是否为1，将数组分为2类，
+        // 此时再将两个数组两两异或就可以得到最终结果。
+        int[] result = new int[2];
+        for (int num : nums) {
+            //将数组分类。
+            if ((num & first) == 0) {
+                result[0] ^= num;
+            } else {
+                result[1] ^= num;
+            }
+        }
+        return result;
+    }
+
+}
---
+++ src/main/java/com/leetcode/iv/offer/Offer56_2.java
@@ -0,0 +1,100 @@
+package com.leetcode.iv.offer;
+
+/**
+ * description 面试题56 - II. 数组中数字出现的次数 II
+ *
+ * 在一个数组 nums 中除一个数字只出现一次之外，其他数字都出现了三次。请找出那个只出现一次的数字。
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [3,4,3,3]
+ * 输出：4
+ * 示例 2：
+ *
+ * 输入：nums = [9,1,7,9,7,9,7]
+ * 输出：1
+ *  
+ *
+ * 限制：
+ *
+ * 1 <= nums.length <= 10000
+ * 1 <= nums[i] < 2^31
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/shu-zu-zhong-shu-zi-chu-xian-de-ci-shu-ii-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author fcz
+ * @since  2020-06-08 10:25
+ **/
+public class Offer56_2 {
+
+    public static void main(String[] args) {
+        Offer56_2 obj = new Offer56_2();
+        System.out.println(obj.singleNumber(new int[]{
+                3,4,3,3
+        }));
+    }
+
+
+    /**
+     * 位运算
+     * 使用二进制的方式，对于出现3次的数字，各 二进制位 出现的次数都是 33 的倍数
+     * 因此，统计所有数字的各二进制位中 11 的出现次数，并对 33 求余，结果则为只出现一次的数字
+     */
+    public int singleNumber(int[] nums) {
+        // int为32数字
+        // 保存各二进制位1出现的次数
+        int[] resArray = new int[32];
+        for (int num : nums) {
+            for (int i = 0; i < 32; i++) {
+                resArray[i] += num & 1;
+                // >>> 高位补0  >> 负数高位补1
+                num >>>= 1;
+            }
+        }
+        // 每一位对3取余后 就是结果 使用 | 运算保存结果
+        int res = 0;
+        for (int i = 0; i < 32; i++) {
+            res <<= 1;
+            res |= resArray[31 - i] % 3;
+        }
+        return res;
+    }
+
+    // 使用hash表计算出现次数 实现简单不写了
+
+
+    /**
+     * 最优解 有限状态自动机 太难了
+     *
+     * 各二进制位的 位运算规则相同 ，因此只需考虑一位即可
+     * 对于所有数字中的某二进制位 1 的个数，存在 3 种状态，即对 3 余数为 0, 1, 2
+     *
+     * 若输入二进制位 1，则状态按照以下顺序转换
+     *      0→1→2→0→⋯
+     * 若输入二进制位 0，则状态不变
+     *
+     * 由于二进制只能表示 0, 1
+     * 因此需要使用两个二进制位来表示 3 个状态
+     * 设此两位分别为 two, one，则状态转换变为：
+     *      00→01→10→00→⋯
+     *
+     * 接下来，需要通过 状态转换表 导出 状态转换的计算公式 。
+     * 首先回忆一下位运算特点，对于任意二进制位 xx ，有：
+     *
+     * 异或运算：x ^ 0 = x​ ， x ^ 1 = ~x
+     * 与运算：x & 0 = 0 ， x & 1 = x
+     *
+     */
+    public int singleNumber1(int[] nums) {
+        int ones = 0, twos = 0;
+        for(int num : nums){
+            ones = ones ^ num & ~twos;
+            twos = twos ^ num & ~ones;
+        }
+        return ones;
+    }
+
+
+}
